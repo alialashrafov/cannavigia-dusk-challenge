aca5a7d5d5c40eedcd12d098122c44c631734d44
ali <alashrafovali@gmail.com>
2022-11-11 18:01:47 +0400

    finished task

--- tests/Browser/AuthenticationTest.php
+++ tests/Browser/AuthenticationTest.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Auth;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 use Faker;
+use Laravel\Fortify\Actions\EnableTwoFactorAuthentication;
+use PragmaRX\Google2FA\Google2FA;
 
 class AuthenticationTest extends DuskTestCase
 {
-    protected string $email;
-    protected string $name;
     /**
      * A basic browser test example.
      *
@@ -27,60 +27,33 @@ class AuthenticationTest extends DuskTestCase
         foreach (static::$browsers as $browser) {
             $browser->driver->manage()->deleteAllCookies();
         }
-        $this->email = 'admin@admin.com';
-        $this->name = 'Someone Else';
 
     }
 
-    public function test_user_register() {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/register')
-                ->type('name', $this->name)
-                ->type('email', $this->email)
-                ->type('password', 'password')
-                ->type('password_confirmation', 'password')
-                ->pause(2000)
-                ->press('REGISTER')
-                ->pause(2000)
-                ->assertPathIs('/dashboard');
-        });
-    }
-    public function test_user_logout()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/dashboard')
-                ->logout()
-                ->assertGuest();
-        });
-    }
+        public function test_user_login(){
 
-        public function test_user_login()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/login')
-                    ->type('email', 'admin@admin.com')
-                    ->type('password', 'password')
-                    ->press('LOG IN')
-                    ->pause(2000)
-                    ->assertPathIs('/dashboard')
-                    ->assertAuthenticated();
-        });
-    }
+            $this->browse(function (Browser $browser) {
+                // Create User with 2FA enabling feature (two_factor_confirmed_at is very important)
+                $user = User::factory()->create(['password' => bcrypt('password'), 'two_factor_confirmed_at' => now()]);
 
-    public function test_user_enable_two_factor() {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/login')
-                    ->type('email', $this->email)
-                    ->type('password', 'password')
-                    ->press('LOG IN')
-                    ->assertPathIs('/dashboard')
-                    ->visit('/user/profile')
-                    ->press("ENABLE")
-                    ->pause(2000)
-                    ->type('confirmable_password', 'password')
-                    ->press('CONFIRM')
-                    ->pause(2000)
-                    ->assertPathIs('/user/profile');
-        });
-    }
+                // generate secret key using Fortify 2FAProvider
+                $tfap = \App::make('Laravel\Fortify\Contracts\TwoFactorAuthenticationProvider');
+                $enable = new EnableTwoFactorAuthentication($tfap);
+                $enablingUser2FA = $enable($user);
+
+                // for getting current OTP, I am using Google2FA package
+                $google2FA = new Google2FA();
+                $currentOtp = $google2FA->getCurrentOtp(decrypt($user->two_factor_secret));
+
+                //That`s all
+                $browser->visit('/login')
+                        ->type('email', $user->email)
+                        ->type('password', 'password')
+                        ->press('LOG IN')
+                        ->type('code', $currentOtp)
+                        ->press('LOG IN')
+                        ->assertPathIs('/dashboard')
+                        ->assertAuthenticated();
+            });
+        }
 }
